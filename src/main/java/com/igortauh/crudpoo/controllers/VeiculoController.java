ae4e1a5f1aa52b4fc13cbe03a1eef65dc85a9628
Igor Tauhan <igortauhan.ds31@gmail.com>
2021-05-23 09:45:34 -0300

    Entidade Cliente criado.

--- src/main/java/com/igortauh/crudpoo/controllers/VeiculoController.java
+++ src/main/java/com/igortauh/crudpoo/controllers/VeiculoController.java
@@ -1,13 +1,14 @@
 package com.igortauh.crudpoo.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/veiculos")
 public class VeiculoController {
 
-    @RequestMapping
+    @RequestMapping(method = RequestMethod.GET)
     public String find() {
         return "O rest está funcionando!";
     }
---
+++ src/main/java/com/igortauh/crudpoo/entities/Cliente.java
@@ -0,0 +1,104 @@
+package com.igortauh.crudpoo.entities;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Date;
+
+public class Cliente implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private Date dataDeNascimento;
+    private String cpfOuCnpj;
+    private String email;
+    private String contato;
+    private String endereco;
+
+    //Associações
+    private Veiculo veiculo;
+
+    public Cliente() {
+
+    }
+
+    public Cliente(Integer id, String nome, Date dataDeNascimento, String cpfOuCnpj, String email, String contato, String endereco, Veiculo veiculo) {
+        this.id = id;
+        this.nome = nome;
+        this.dataDeNascimento = dataDeNascimento;
+        this.cpfOuCnpj = cpfOuCnpj;
+        this.email = email;
+        this.contato = contato;
+        this.endereco = endereco;
+        this.veiculo = veiculo;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+
+    public void setDataDeNascimento(Date dataDeNascimento) {
+        this.dataDeNascimento = dataDeNascimento;
+    }
+
+    public String getCpfOuCnpj() {
+        return cpfOuCnpj;
+    }
+
+    public void setCpfOuCnpj(String cpfOuCnpj) {
+        this.cpfOuCnpj = cpfOuCnpj;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public Veiculo getVeiculo() {
+        return veiculo;
+    }
+
+    public void setVeiculo(Veiculo veiculo) {
+        this.veiculo = veiculo;
+    }
+}
